969ba4fbf0defee6845ecc37e07a40c81255fe7d
w-pepro <willy.tan@pepro.io>
2025-04-07 17:09:27 +0200

    test

--- app.config.js
+++ app.config.js
@@ -12,9 +12,9 @@ module.exports = {
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
   chainIds: [5, 1, 137, 10],
-  infuraProjectId:
-    process.env.NEXT_PUBLIC_INFURA_PROJECT_ID ||
-    '5ffc47f65c4042ce847ef66a3fa70d4c',
+  infuraProjectId: 
+    '6a3e18dfad90440aa5ad64e8f71d9da9'
+  // process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
   // List of all supported chainIds. Used to populate the Chains user preferences list.
   chainIdsSupported: [1, 137, 10, 5, 80001, 11155111],
 
